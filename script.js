791d0dfdf41e4f05f98cf34e7eda0a836d38a0ad
QuintinPearson <qpearson1999@gmail.com>
2021-10-07 16:17:12 +0200

    footer added

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ let slide = document.getElementById("slide");
 
 btn[0].addEventListener("click", function () {
   slide.style.transform = "translateX(0px)";
+  for (i = 0; i < 3; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
 });
 
 btn[1].addEventListener("click", function () {
-  slide.style.transform = "translateX(-1150px)";
+  slide.style.transform = "translateX(-100vw)";
+  for (i = 0; i < 3; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
 });
 
 btn[2].addEventListener("click", function () {
-  slide.style.transform = "translateX(-2300px)";
+  slide.style.transform = "translateX(-200vw)";
+  for (i = 0; i < 3; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
 });
-
-console.log(btn);
