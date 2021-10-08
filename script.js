e9ccc140c563f61619a886b958118dd180b0b0b7
QuintinPearson <qpearson1999@gmail.com>
2021-10-08 15:49:01 +0200

    mobile nav and effects added

--- script.js
+++ script.js
@@ -24,3 +24,47 @@ btn[2].addEventListener("click", function () {
   }
   this.classList.add("active");
 });
+
+// current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// ----------------------
+// Mobile navigation
+// ----------------------
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector("header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// ----------------------
+// Smooth Scrolling animation for safari
+// ----------------------
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close the mobile nav
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
