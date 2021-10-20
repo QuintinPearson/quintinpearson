a99f1418afa85df2bae705c1b7b53e2f40cd57ea
QuintinPearson <qpearson1999@gmail.com>
2021-10-20 09:16:21 +0200

    seperate folder added for contact page

--- contact/contact.js
+++ contact/contact.js
@@ -1,45 +1,36 @@
-let btn = document.getElementsByClassName("carousel-btn");
-let slide = document.getElementById("slide");
-
-btn[0].addEventListener("click", function () {
-  slide.style.transform = "translateX(0px)";
-  for (i = 0; i < 3; i++) {
-    btn[i].classList.remove("active");
-  }
-  this.classList.add("active");
-});
-
-btn[1].addEventListener("click", function () {
-  slide.style.transform = "translateX(-100vw)";
-  for (i = 0; i < 3; i++) {
-    btn[i].classList.remove("active");
-  }
-  this.classList.add("active");
-});
-
-btn[2].addEventListener("click", function () {
-  slide.style.transform = "translateX(-200vw)";
-  for (i = 0; i < 3; i++) {
-    btn[i].classList.remove("active");
-  }
-  this.classList.add("active");
-});
-
 // current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 // ----------------------
-// Mobile navigation
+// Form spree contact form
 // ----------------------
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector("header");
+var form = document.getElementById("my-form");
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.classList.add("success");
+      status.innerHTML = "Thanks for your submission!";
+      form.reset();
+    })
+    .catch((error) => {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
 
 // ----------------------
 // Smooth Scrolling animation for safari
